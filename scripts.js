068fb859c2e040d1e9a56661acf03ca60085adb6
thecocou <thecocou@gmail.com>
2017-08-09 13:49:28 -0300

    .

--- scripts.js
+++ scripts.js
@@ -3,12 +3,24 @@
 function initRouter(){
   var Mapa = cargarMapa();  // Cargo Mapa
       Geocoder = new google.maps.Geocoder();
+      enrutador = new google.maps.DirectionsService;
+      displayRoutes = new google.maps.DirectionsRenderer({map: Mapa});
       botonEnrutar = document.getElementById('enrutar');
+      direccion = [];
+
+      console.log(direccion);
 
   // Al hacer click en buscar geocodificar la direccion
   botonEnrutar.addEventListener("click", function() {
-    Rutas = new Ruta(Mapa)
-      .enrutarDirecciones(Geocoder, Zonas);
+
+    for (i = 0; i < 23; i++) {
+      if(document.getElementById('direccion['i']').value !== ""){
+        var direccion[i] = document.getElementById('direccion['i']').value;
+      }
+    }
+    console.log(direccion);
+
+//    mostrarRuta(direccion[0], direccion[direccion.lenght]);
 
     setearOpcionesDelMapaPorDefault(Mapa);
     setearCursorEnCampoDireccion();
@@ -25,91 +37,59 @@ function cargarMapa() {
   });
   return Mapa;
 }
+/*
+function mostrarRuta(origin, destination) {
+  enrutador.route({
+    origin: "Capital Federal",
+    destination: "Capital Federal",
+    travelMode: "WALKING",
+    transitOptions: {routingPreference: "LESS_WALKING"},
+    waypoints[3]: DirectionsWaypoint: [
+      {document.getElementById('direccion[0]').value},
+      {document.getElementById('direccion[1]').value},
+      {document.getElementById('direccion[2]').value}
+    ],
+    optimizeWaypoints: true,
+    region: "AR"
+  }, function(response, status) {
+    if (status === 'OK') {
+      displayRoutes.setDirections(response);
+    } else {
+      alert('No es posible mostrar la ruta por lo siguiente: ' + status);
+    }
+  });
+}
+*/
+/*
+function computeTotalDistance(result) {
+  var total = 0;
+  var myroute = result.routes[0];
+  for (var i = 0; i < myroute.legs.length; i++) {
+    total += myroute.legs[i].distance.value;
+  }
+  total = total / 1000;
+  document.getElementById('total').innerHTML = total + ' km';
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
-
-var DirectionsRequest = {
-  origin: "Capital Federal",
-  destination: "Capital Federal",
-  travelMode: "WALKING",
-  transitOptions: {routingPreference: "LESS_WALKING"},
-  waypoints[3]: DirectionsWaypoint: [
-    {document.getElementById('direccion[0]').value}
-    {document.getElementById('direccion[1]').value}
-    {document.getElementById('direccion[2]').value}
-  ],
-  optimizeWaypoints: true,
-  region: "AR"
-}
-
-<script>
-      function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: {lat: -24.345, lng: 134.46}  // Australia.
-        });
-
-        var directionsService = new google.maps.DirectionsService;
-        var directionsDisplay = new google.maps.DirectionsRenderer({
-          draggable: true,
-          map: map,
-          panel: document.getElementById('right-panel')
-        });
-
-        directionsDisplay.addListener('directions_changed', function() {
-          computeTotalDistance(directionsDisplay.getDirections());
-        });
-
-        displayRoute('Perth, WA', 'Sydney, NSW', directionsService,
-            directionsDisplay);
-      }
-
-      function displayRoute(origin, destination, service, display) {
-        service.route({
-          origin: origin,
-          destination: destination,
-          waypoints: [{location: 'Adelaide, SA'}, {location: 'Broken Hill, NSW'}],
-          travelMode: 'DRIVING',
-          avoidTolls: true
-        }, function(response, status) {
-          if (status === 'OK') {
-            display.setDirections(response);
-          } else {
-            alert('Could not display directions due to: ' + status);
-          }
-        });
-      }
-
-      function computeTotalDistance(result) {
-        var total = 0;
-        var myroute = result.routes[0];
-        for (var i = 0; i < myroute.legs.length; i++) {
-          total += myroute.legs[i].distance.value;
-        }
-        total = total / 1000;
-        document.getElementById('total').innerHTML = total + ' km';
-      }
-    </script>
-
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // CLASE RUTA
 class Ruta {
   constructor(Mapa) {
@@ -155,7 +135,7 @@ class Ruta {
     element.scrollIntoView(false);
     return this;
   }
-}
+}*/
 
 // Funcion para limpiar los inputs
 function blanquearInputsYTips(){
