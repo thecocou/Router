b1367981fba1a7f6770af0d7c8701ebee81e25ca
thecocou <lautarobestard@gmail.com>
2018-10-02 15:23:37 -0300

    time fix

--- scripts.js
+++ scripts.js
@@ -101,7 +101,7 @@ function calcularTiempoyDistancia(respuesta){
   let distancia = document.getElementById("distancia");
   let tiempo = document.getElementById("tiempo");
 
-  tiempo.innerHTML = "<p>TIEMPO ESTIMADO: " + (totalTime / 60).toFixed(2) + " minutos</p>"
+  tiempo.innerHTML = "<p>TIEMPO ESTIMADO: " + (totalTime / 60).toFixed(0) + " minutos</p>"
   distancia.innerHTML = "<p> DISTANCIA TOTAL: " + (totalDist / 1000).toFixed(1) + " km</p>";
 } 
 
